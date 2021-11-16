1ae854fcc45b1d14bab9fe2736093e8aabbce6ea
su-tran75 <su-tran@hotmail.fr>
2021-11-16 17:28:42 +0800

    Install @material-ui to handle icons and create topBar left side UI

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import "./topBar.scss";
+import { Person, Email } from "@mui/icons-material";
 
 export default function TopBar() {
-  return <div className="topBar"></div>;
+  return (
+    <div className="topBar">
+      <div className="wrapper">
+        <div className="left">
           <a href="#intro" className="logo">
             Home
           </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+33 710 10 10 10</span>
+          </div>
+          <div className="itemContainer">
+            <Email className="icon" />
+            <span>contact@gmail.com</span>
+          </div>
+        </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
