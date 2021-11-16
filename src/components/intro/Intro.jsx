c6505a940363ae0253f15df4248ffc223ea25d81
su-tran75 <su-tran@hotmail.fr>
2021-11-16 19:08:36 +0800

    Create Intro.jsx and add style

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,23 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There! I'm</h2>
+          <h1>John DOE</h1>
+          <h3>
+            Front-end developer<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
