a0d2c2f9cea1eb075666f72c0279843448979948
su-tran75 <su-tran@hotmail.fr>
2021-11-17 18:43:07 +0800

    Create Contact.jsx and add style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea name="" placeholder="Message"></textarea>
+          <button type="submit">
+            {!message ? "Send" : "Done"}
+          </button>
+          {message && <span>Thanks, I'll reply ASAP</span>}
+        </form>
+      </div>
     </div>
   );
 }
