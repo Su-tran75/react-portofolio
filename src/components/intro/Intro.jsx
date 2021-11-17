079291f312d767afec19482a6553fd239b94c68f
su-tran75 <su-tran@hotmail.fr>
2021-11-17 10:50:55 +0800

    Use Ityped lib to create text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["React", "Developer", "Yeah!"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +26,7 @@ export default function Intro() {
           <h2>Hi There! I'm</h2>
           <h1>John DOE</h1>
           <h3>
-            Front-end developer<span></span>
+            Front-End <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
